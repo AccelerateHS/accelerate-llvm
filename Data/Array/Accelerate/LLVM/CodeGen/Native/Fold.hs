b0db7d1587f58b4beb38eb7d0f108185ac5037da
Trevor L. McDonell <tmcdonell@nvidia.com>
2014-02-19 12:07:15 -0800

    code generation for cooperative foldAll
    Add code generation support for reducing an array to a single value using multiple threads. This is executed in two passes, where each thread returns its partial result into a temporary array, which is then reduced sequentially by a single thread.

--- Data/Array/Accelerate/LLVM/CodeGen/Native/Fold.hs
+++ Data/Array/Accelerate/LLVM/CodeGen/Native/Fold.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP                 #-}
 {-# LANGUAGE GADTs               #-}
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ParallelListComp    #-}
@@ -22,6 +23,8 @@ import LLVM.General.AST
 import LLVM.General.AST.Global
 
 -- accelerate
+import Data.Array.Accelerate.AST
+import Data.Array.Accelerate.Analysis.Shape
 import Data.Array.Accelerate.Array.Sugar
 import Data.Array.Accelerate.Type
 
@@ -35,8 +38,55 @@ import Data.Array.Accelerate.LLVM.CodeGen.Monad
 import Data.Array.Accelerate.LLVM.CodeGen.Type
 
 -- standard library
+import Control.Applicative
 import Control.Monad
+import GHC.Conc
 
+#include "accelerate.h"
+
+
+-- Reduce an array along the innermost dimension
+--
+mkFold
+    :: forall t aenv sh e. (Shape sh, Elt e)
+    => Gamma aenv
+    -> IRFun2    aenv (e -> e -> e)
+    -> IRExp     aenv e
+    -> IRDelayed aenv (Array (sh:.Int) e)
+    -> CodeGen [Kernel t aenv (Array sh e)]
+mkFold aenv f z a
+  -- Either (1) multidimensional fold; or
+  --        (2) only using one CPU, so just execute sequentially
+  | numCapabilities <= 1 || expDim (undefined::Exp aenv sh) > 0
+  = mkFold' aenv f z a
+
+  -- Parallel foldAll
+  | otherwise
+  = (++) <$> mkFold1' aenv f a <*> mkFoldAll' aenv f z
+
+
+-- Reduce an array along the innermost dimension. The innermost dimension must
+-- not be empty.
+--
+mkFold1
+    :: forall t aenv sh e. (Shape sh, Elt e)
+    => Gamma aenv
+    -> IRFun2    aenv (e -> e -> e)
+    -> IRDelayed aenv (Array (sh:.Int) e)
+    -> CodeGen [Kernel t aenv (Array sh e)]
+mkFold1 aenv f a
+  -- Either (1) multidimensional fold; or
+  --        (2) only using one CPU, so just execute sequentially
+  | numCapabilities <= 1 || expDim (undefined::Exp aenv sh) > 0
+  = mkFold1' aenv f a
+
+  -- Parallel foldAll
+  | otherwise
+  = (++) <$> mkFold1' aenv f a <*> mkFold1All' aenv f
+
+
+-- Multidimensional reduction
+-- --------------------------
 
 -- Reduce a multidimensional array. Threads sequentially reduce along the
 -- innermost dimensions, so don't need to communicate. Each thread is given a
@@ -51,14 +101,14 @@ import Control.Monad
 -- >           writeArray out sh (reduce indexArray c z sz next)
 -- >           iter (sh+1) next
 --
-mkFold
+mkFold'
     :: forall t aenv sh e. (Shape sh, Elt e)
     => Gamma aenv
     -> IRFun2    aenv (e -> e -> e)
     -> IRExp     aenv e
     -> IRDelayed aenv (Array (sh:.Int) e)
     -> CodeGen [Kernel t aenv (Array sh e)]
-mkFold aenv combine seed IRDelayed{..} = do
+mkFold' aenv combine seed IRDelayed{..} = do
   code <- runBody
   return [ Kernel $ functionDefaults
              { returnType  = VoidType
@@ -113,17 +163,17 @@ mkFold aenv combine seed IRDelayed{..} = do
 -- provided, initialise the loop accumulator with the first element of the
 -- array.
 --
-mkFold1
+mkFold1'
     :: forall t aenv sh e. (Shape sh, Elt e)
     => Gamma aenv
     -> IRFun2    aenv (e -> e -> e)
     -> IRDelayed aenv (Array (sh:.Int) e)
     -> CodeGen [Kernel t aenv (Array sh e)]
-mkFold1 aenv combine IRDelayed{..} = do
+mkFold1' aenv combine IRDelayed{..} = do
   code <- runBody
   return [ Kernel $ functionDefaults
              { returnType  = VoidType
-             , name        = "fold"
+             , name        = "fold1"
              , parameters  = (gang ++ paramIn ++ paramOut, False)
              , basicBlocks = code
              } ]
@@ -164,6 +214,109 @@ mkFold1 aenv combine IRDelayed{..} = do
       _         <- phi loop c_sh (typeOf (int :: IntegralType Int)) [ (start,top), (sh',bot) ]
 
       setBlock exit
+      return_
+      >>
+      createBlocks
+
+
+-- Scalar reduction
+-- ----------------
+
+-- Reduce an array to a single element, with all threads cooperating.
+--
+-- Since reductions consume arrays that have been fused into them, fold in a
+-- multi-threaded context requires two passes. At an example, take vector
+-- dot-product:
+--
+-- > dotp xs ys = fold (+) 0 (zipWith (*) xs ys)
+--
+--   1. The first pass reads in the fused array data, in this case corresponding
+--   to the function (\i -> (xs!i) * (ys!i)).
+--
+--   2. The second pass just reads the manifest data from the first step
+--   directly and reduces the array. This second step is just done by a single
+--   thread.
+--
+mkFoldAll'
+    :: forall t aenv sh e. (Shape sh, Elt e)
+    => Gamma aenv
+    -> IRFun2 aenv (e -> e -> e)
+    -> IRExp  aenv e
+    -> CodeGen [Kernel t aenv (Array sh e)]             -- types are bit odd, but required
+mkFoldAll' aenv combine seed = do
+  code <- runBody
+  return [ Kernel $ functionDefaults
+             { returnType  = VoidType
+             , name        = "foldAll"
+             , parameters  = (paramIn ++ paramOut, False)
+             , basicBlocks = code
+             } ]
+  where
+    paramIn             = envParam aenv ++
+                          arrayParam (undefined::Array (sh:.Int) e) "in"
+    arrIn               = arrayData  (undefined::Array (sh:.Int) e) "in"
+    shIn                = arrayShape (undefined::Array (sh:.Int) e) "in"
+
+    paramOut            = arrayParam (undefined::Array sh e) "out"
+    arrOut              = arrayData  (undefined::Array sh e) "out"
+
+    ty_acc              = llvmOfTupleType (eltType (undefined::e))
+    get [i]             = readArray arrIn i
+    get _               = INTERNAL_ERROR(error) "makeFoldAll" "expected single expression"
+
+    one                 = constOp (num int 1)
+    zero                = constOp (num int 0)
+
+    runBody :: CodeGen [BasicBlock]
+    runBody = do
+      sh        <- foldM (mul int) one (map local shIn)
+      r         <- reduce ty_acc get combine seed zero sh
+      writeArray arrOut zero r
+      return_
+      >>
+      createBlocks
+
+
+-- Reduce an array to a single element, without a starting value.
+--
+-- This is just the second phase of the operation, which takes the manifest
+-- array of partial summations from each thread and reduces them to a single
+-- value.
+--
+mkFold1All'
+    :: forall t aenv sh e. (Shape sh, Elt e)
+    => Gamma aenv
+    -> IRFun2 aenv (e -> e -> e)
+    -> CodeGen [Kernel t aenv (Array sh e)]
+mkFold1All' aenv combine = do
+  code <- runBody
+  return [ Kernel $ functionDefaults
+             { returnType  = VoidType
+             , name        = "foldAll"
+             , parameters  = (paramIn ++ paramOut, False)
+             , basicBlocks = code
+             } ]
+  where
+    paramIn             = envParam aenv ++
+                          arrayParam (undefined::Array (sh:.Int) e) "in"
+    arrIn               = arrayData  (undefined::Array (sh:.Int) e) "in"
+    shIn                = arrayShape (undefined::Array (sh:.Int) e) "in"
+
+    paramOut            = arrayParam (undefined::Array sh e) "out"
+    arrOut              = arrayData  (undefined::Array sh e) "out"
+
+    ty_acc              = llvmOfTupleType (eltType (undefined::e))
+    get [i]             = readArray arrIn i
+    get _               = INTERNAL_ERROR(error) "makeFoldAll" "expected single expression"
+
+    one                 = constOp (num int 1)
+    zero                = constOp (num int 0)
+
+    runBody :: CodeGen [BasicBlock]
+    runBody = do
+      sh        <- foldM (mul int) one (map local shIn)
+      r         <- reduce ty_acc get combine (get [zero]) one sh
+      writeArray arrOut zero r
       return_
       >>
       createBlocks
