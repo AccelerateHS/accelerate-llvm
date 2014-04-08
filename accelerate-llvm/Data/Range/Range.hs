d72569e87e5358172d9cbb68f1fe3c2a64b27b91
Trevor L. McDonell <tmcdonell@nvidia.com>
2014-04-08 11:23:27 -0700

    operations for working with a sequence of ranges

--- accelerate-llvm/Data/Range/Range.hs
+++ accelerate-llvm/Data/Range/Range.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP          #-}
 {-# LANGUAGE BangPatterns #-}
 {-# OPTIONS_GHC -funbox-strict-fields #-}
 -- |
@@ -13,9 +14,14 @@
 module Data.Range.Range
   where
 
-import Prelude                                  hiding ( take, splitAt )
+import Prelude                                          hiding ( take, splitAt )
+import GHC.Base                                         ( quotInt )
 import Text.Printf
-import GHC.Base                                 ( quotInt )
+
+import Data.Sequence                                    ( Seq )
+import qualified Data.Sequence                          as Seq
+
+#include "accelerate.h"
 
 
 -- | A simple range data type
@@ -60,7 +66,7 @@ u ... v
 infix 3 ...
 
 
--- | The number of elements defined by the range interval
+-- | /O(1)/. The number of elements defined by the range interval
 --
 {-# INLINE size #-}
 size :: Range -> Int
@@ -70,7 +76,7 @@ size range =
     IE u v      -> v - u
 
 
--- | Split an interval into two roughly equally sized ranges. If the interval is
+-- | /O(1)/. Split an interval into two roughly equally sized ranges. If the interval is
 -- odd then the first interval gets the extra element.
 --
 {-# INLINE bisect #-}
@@ -90,7 +96,7 @@ bisect range =
       (x, y)
 
 
--- | Return the first @n@ elements of the range, or the range itself if
+-- | /O(1)/. Return the first @n@ elements of the range, or the range itself if
 -- @n > size@.
 --
 {-# INLINE take #-}
@@ -102,7 +108,7 @@ take !n !range =
     IE u v -> IE u ((u+n) `min` v)
 
 
--- | A tuple where the first element is the first @n@ elements of the range, and
+-- | /O(1)/. A tuple where the first element is the first @n@ elements of the range, and
 -- the second is the remainder of the list (if any).
 --
 {-# INLINE splitAt #-}
@@ -118,4 +124,56 @@ splitAt !n !range =
             | otherwise = Empty
       in
       (x, y)
+
+
+-- | If the two ranges are adjacent, return one combined range. The ranges must
+-- not be empty.
+--
+{-# INLINE merge #-}
+merge :: Range -> Range -> Maybe Range
+merge (IE u v) (IE x y)
+  | v == x      = Just (IE u y)
+  | otherwise   = Nothing
+merge _ _       = INTERNAL_ERROR(error) "merge" "empty range encountered"
+
+
+-- | /O(1)/. Add a new range to the end of the given sequence. We assume that
+-- ranges are non-overlapping and non-empty. If the new range is adjacent to the
+-- last range on the sequence, the ranges are appended.
+--
+{-# INLINEABLE append #-}
+append :: Seq Range -> Range -> Seq Range
+append rs Empty = rs
+append rs next  =
+  case Seq.viewr rs of
+    Seq.EmptyR                          -> Seq.singleton next
+    rs' Seq.:> prev
+      | Just r <- merge prev next       -> rs' Seq.|> r
+      | otherwise                       -> rs  Seq.|> next
+
+
+-- | /O(n log n)/. Compress the given ranges into the fewest number of sections
+-- as possible. The ranges must not be empty.
+--
+{-# INLINEABLE compress #-}
+compress :: Seq Range -> Seq Range
+compress = squash . Seq.unstableSortBy cmp
+  where
+    -- Compare by the lower bound. Assume ranges are non-overlapping.
+    --
+    cmp (IE u _) (IE v _) = compare u v
+    cmp _        _        = INTERNAL_ERROR(error) "compress" "empty range encountered"
+
+    -- Look at the first two elements, compress them if they are adjacent, and
+    -- continue walking down the sequence doing the same. If we merge a range,
+    -- be sure to continue attempting to merge that with subsequent ranges
+    --
+    squash rrs =
+      case Seq.viewl rrs of
+        Seq.EmptyL      -> Seq.empty
+        r1 Seq.:< rs    -> case Seq.viewl rs of
+                             Seq.EmptyL                     -> rrs
+                             r2 Seq.:< rs'
+                               | Just r12 <- merge r1 r2    -> squash $ r12 Seq.<| rs'
+                               | otherwise                  ->          r1  Seq.<| squash rs
 
