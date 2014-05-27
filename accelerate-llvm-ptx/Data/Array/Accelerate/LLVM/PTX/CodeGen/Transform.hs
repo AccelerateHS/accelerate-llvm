8c356e41be2268bcb6e7015d06a95c9463296d58
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2014-05-27 17:47:06 +1000

    Revert: use monadic-style ptx transform kernel

--- accelerate-llvm-ptx/Data/Array/Accelerate/LLVM/PTX/CodeGen/Transform.hs
+++ accelerate-llvm-ptx/Data/Array/Accelerate/LLVM/PTX/CodeGen/Transform.hs
@@ -1,9 +1,8 @@
 {-# LANGUAGE GADTs               #-}
 {-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RankNTypes          #-}
 {-# LANGUAGE RecordWildCards     #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE QuasiQuotes         #-}
-{-# LANGUAGE RankNTypes          #-}
 -- |
 -- Module      : Data.Array.Accelerate.LLVM.PTX.CodeGen.Transform
 -- Copyright   : [2014] Trevor L. McDonell, Sean Lee, Vinod Grover, NVIDIA Corporation
@@ -19,8 +18,8 @@ module Data.Array.Accelerate.LLVM.PTX.CodeGen.Transform
 
 -- accelerate
 import Data.Array.Accelerate.Array.Sugar                        ( Array, Shape, Elt )
-import Data.Array.Accelerate.Type
 
+import Data.Array.Accelerate.LLVM.CodeGen.Arithmetic
 import Data.Array.Accelerate.LLVM.CodeGen.Base
 import Data.Array.Accelerate.LLVM.CodeGen.Environment
 import Data.Array.Accelerate.LLVM.CodeGen.Exp
@@ -30,9 +29,7 @@ import Data.Array.Accelerate.LLVM.CodeGen.Type
 
 import Data.Array.Accelerate.LLVM.PTX.Target                    ( PTX )
 import Data.Array.Accelerate.LLVM.PTX.CodeGen.Base
-
-import LLVM.General.AST
-import LLVM.General.Quote.LLVM
+import Data.Array.Accelerate.LLVM.PTX.CodeGen.Loop
 
 -- standard library
 import Prelude                                                  hiding ( fromIntegral )
@@ -51,38 +48,21 @@ mkTransform
     -> CodeGen [Kernel t aenv (Array sh' b)]
 mkTransform _dev aenv permute apply IRDelayed{..} =
   let
+      (start, end, paramGang)   = gangParam
       arrOut                    = arrayData  (undefined::Array sh' b) "out"
       shOut                     = arrayShape (undefined::Array sh' b) "out"
       paramOut                  = arrayParam (undefined::Array sh' b) "out"
       paramEnv                  = envParam aenv
-      (start, end, paramGang)   = gangParam
-      intType                   = (typeOf (integralType :: IntegralType Int))
   in
-  makeKernelQ "transform" [llgM|
-    define void @transform
-    (
-        $params:paramGang,
-        $params:paramOut,
-        $params:paramEnv
-    )
-    {
-        $bbsM:("step" .=. gridSize)
-        $bbsM:("tid"  .=. globalThreadIdx)
-        %z = add i32 %tid, $opr:start
-        br label %nextblock
+  makeKernel "transform" (paramGang ++ paramOut ++ paramEnv) $ do
 
-        for i32 %i in %z to $opr:end step %step
-        {
-            %ii = sext i32 %i to $type:intType                  ;; loop counter is i32, calculation is in Int
-            br label %nextblock
+    imapFromTo start end $ \i -> do
+      ii  <- fromIntegral int32 int i           -- loop counter is i32, calculation is in Int
+      ix  <- indexOfInt shOut ii                -- convert to multidimensional index
+      ix' <- permute ix                         -- apply backwards index permutation
+      xs  <- delayedIndex ix'                   -- get element
+      ys  <- apply xs                           -- apply function from input array
+      writeArray arrOut i ys
 
-            $bbsM:("ix"  .=. indexOfInt shOut ("ii" :: Name))   ;; convert to multidimensional index
-            $bbsM:("ix1" .=. permute ("ix" :: Name))            ;; apply backwards index permutation
-            $bbsM:("xs"  .=. delayedIndex ("ix1" :: Name))      ;; get element
-            $bbsM:("ys"  .=. apply ("xs" :: Name))              ;; apply function from input array
-            $bbsM:(execRet_ (writeArray arrOut "i" ("ys" :: Name)))
-        }
-        ret void
-    }
-  |]
+    return_
 
