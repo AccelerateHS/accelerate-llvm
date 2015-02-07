c9c6bca6b563f2562e3e6c39b6d4d2f3dc3c8230
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2015-02-07 14:58:02 -0500

    code generation for array expressions

--- accelerate-llvm/Data/Array/Accelerate/LLVM/CodeGen.hs
+++ accelerate-llvm/Data/Array/Accelerate/LLVM/CodeGen.hs
@@ -1,11 +1,10 @@
-{-# LANGUAGE CPP                 #-}
+{-# LANGUAGE FlexibleContexts    #-}
 {-# LANGUAGE GADTs               #-}
 {-# LANGUAGE RecordWildCards     #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell     #-}
 {-# LANGUAGE TypeOperators       #-}
 {-# LANGUAGE ViewPatterns        #-}
-{-# LANGUAGE RankNTypes          #-}
 -- |
 -- Module      : Data.Array.Accelerate.LLVM.CodeGen
 -- Copyright   : [2015] Trevor L. McDonell
@@ -18,7 +17,8 @@
 
 module Data.Array.Accelerate.LLVM.CodeGen (
 
-  -- Skeleton(..), Intrinsic(..), llvmOfAcc,
+  Skeleton(..), Expression(..), Intrinsic(..),
+  llvmOfOpenAcc,
 
 ) where
 
@@ -36,24 +36,24 @@ import Data.Array.Accelerate.LLVM.CodeGen.Environment
 import Data.Array.Accelerate.LLVM.CodeGen.Exp
 import Data.Array.Accelerate.LLVM.CodeGen.IR
 import Data.Array.Accelerate.LLVM.CodeGen.Intrinsic
+import Data.Array.Accelerate.LLVM.CodeGen.Module
 import Data.Array.Accelerate.LLVM.CodeGen.Monad
 import Data.Array.Accelerate.LLVM.CodeGen.Skeleton
 
 -- standard library
 import Prelude                                                  hiding ( map, scanl, scanl1, scanr, scanr1 )
-import qualified Prelude                                        as P
 
 
-{--
 -- | Generate code for a given target architecture.
 --
-llvmOfAcc :: forall arch aenv arrs. (Target arch, Skeleton arch, Intrinsic arch)
-          => arch
-          -> DelayedOpenAcc aenv arrs
-          -> Val aenv
-          -> Module arch aenv arrs
-llvmOfAcc _    Delayed{}       _    = $internalError "llvmOfAcc" "expected manifest array"
-llvmOfAcc arch (Manifest pacc) aenv = runLLVM $
+llvmOfOpenAcc
+    :: forall arch aenv arrs. (Target arch, Skeleton arch, Intrinsic arch, Expression arch)
+    => arch
+    -> DelayedOpenAcc aenv arrs
+    -> Val aenv
+    -> Module arch aenv arrs
+llvmOfOpenAcc _    Delayed{}       _    = $internalError "llvmOfOpenAcc" "expected manifest array"
+llvmOfOpenAcc arch (Manifest pacc) aenv = runLLVM $
   case pacc of
     -- Producers
     Map f a                 -> map arch aenv (travF1 f) (travD a)
@@ -73,8 +73,8 @@ llvmOfAcc arch (Manifest pacc) aenv = runLLVM $
     Scanr' f z a            -> scanr' arch aenv (travF2 f) (travE z) (travD a)
     Scanr1 f a              -> scanr1 arch aenv (travF2 f) (travD a)
     Permute f _ p a         -> permute arch aenv (travF2 f) (travF1 p) (travD a)
-    Stencil f b a           -> stencil arch aenv (travF1 f) (travB a b)
-    Stencil2 f b1 a1 b2 a2  -> stencil2 arch aenv (travF2 f) (travB a1 b1) (travB a2 b2)
+    Stencil f b a           -> stencil arch aenv (travF1 f) (travB a b) (travM a)
+    Stencil2 f b1 a1 b2 a2  -> stencil2 arch aenv (travF2 f) (travB a1 b1) (travM a1) (travB a2 b2) (travM a2)
 
     -- Non-computation forms: sadness
     Alet{}                  -> unexpectedError pacc
@@ -95,19 +95,23 @@ llvmOfAcc arch (Manifest pacc) aenv = runLLVM $
 
   where
     -- code generation for delayed arrays
-    travD :: DelayedOpenAcc aenv (Array sh e) -> IRDelayed aenv (Array sh e)
-    travD Manifest{}  = $internalError "llvmOfAcc" "expected delayed array"
+    travD :: DelayedOpenAcc aenv (Array sh e) -> IRDelayed arch aenv (Array sh e)
+    travD Manifest{}  = $internalError "llvmOfOpenAcc" "expected delayed array"
     travD Delayed{..} = IRDelayed (travE extentD) (travF1 indexD) (travF1 linearIndexD)
 
-    -- scalar code generation
-    travF1 :: DelayedFun aenv (a -> b) -> IRFun1 aenv (a -> b)
-    travF1 f = llvmOfFun1 f aenv
+    travM :: DelayedOpenAcc aenv (Array sh e) -> IRManifest arch aenv (Array sh e)
+    travM (Manifest (Avar ix)) = IRManifest ix
+    travM _                    = $internalError "llvmOfOpenAcc" "expected manifest array variable"
 
-    travF2 :: DelayedFun aenv (a -> b -> c) -> IRFun arch aenv (a -> b -> c)
-    travF2 f = llvmOfFun2 f aenv
+    -- scalar code generation
+    travF1 :: DelayedFun aenv (a -> b) -> IRFun1 arch aenv (a -> b)
+    travF1 f = llvmOfFun1 arch f aenv
+
+    travF2 :: DelayedFun aenv (a -> b -> c) -> IRFun2 arch aenv (a -> b -> c)
+    travF2 f = llvmOfFun2 arch f aenv
 
     travE :: DelayedExp aenv t -> IRExp arch aenv t
-    travE e = llvmOfOpenExp e Empty aenv
+    travE e = llvmOfOpenExp arch e Empty aenv
 
     travB :: forall sh e. Elt e
           => DelayedOpenAcc aenv (Array sh e)
@@ -117,18 +121,17 @@ llvmOfAcc arch (Manifest pacc) aenv = runLLVM $
     travB _ Mirror       = Mirror
     travB _ Wrap         = Wrap
     travB _ (Constant c)
-      = Constant $ return
-      $ P.map constOp (constant (eltType (undefined::e)) c)
+      = Constant . return
+      $ IR (constant (eltType (undefined::e)) c)
 
     -- sadness
-    unexpectedError x   = $internalError "llvmOfAcc" $ "unexpected array primitive: " ++ showPreAccOp x
-    fusionError x       = $internalError "llvmOfAcc" $ "unexpected fusible material: " ++ showPreAccOp x
---}
-
--- Default producers
--- -----------------
+    unexpectedError x   = $internalError "llvmOfOpenAcc" $ "unexpected array primitive: "  ++ showPreAccOp x
+    fusionError x       = $internalError "llvmOfOpenAcc" $ "unexpected fusible material: " ++ showPreAccOp x
 
 
--- defaultStencil
---     :: (Shape sh, Elt a, Skeleton arch)
+runLLVM
+    :: forall arch aenv a. (Target arch, Intrinsic arch)
+    => CodeGen (IROpenAcc arch aenv a)
+    -> Module arch aenv a
+runLLVM = undefined
 
