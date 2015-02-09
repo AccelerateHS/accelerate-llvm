344423c53c75c890a91068e1c517a527842ab769
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2015-02-09 11:37:52 -0500

    reading array elements

--- accelerate-llvm/Data/Array/Accelerate/LLVM/CodeGen/Array.hs
+++ accelerate-llvm/Data/Array/Accelerate/LLVM/CodeGen/Array.hs
@@ -13,9 +13,13 @@
 
 module Data.Array.Accelerate.LLVM.CodeGen.Array (
 
+  readArray,
   writeArray,
 
 ) where
+
+import Prelude                                                          hiding ( read )
+import Control.Applicative
 
 import LLVM.General.AST.Type.Operand
 import LLVM.General.AST.Type.Instruction
@@ -28,11 +32,33 @@ import Data.Array.Accelerate.LLVM.CodeGen.Monad
 import Data.Array.Accelerate.LLVM.CodeGen.Sugar
 
 
+
+-- | Read a value from an array at the given index
+--
+readArray :: forall sh e. IRArray (Array sh e) -> IR Int -> CodeGen (IR e)
+readArray (IRArray (IR adata)) (op integralType -> ix) =
+  IR <$> readArrayData NonVolatile ix (eltType (undefined::e)) adata
+
+readArrayData :: Volatile -> Operand Int -> TupleType t -> Operands t -> CodeGen (Operands t)
+readArrayData volatile ix = read
+  where
+    read :: TupleType t -> Operands t -> CodeGen (Operands t)
+    read UnitTuple          OP_Unit        = return OP_Unit
+    read (PairTuple t2 t1) (OP_Pair a2 a1) = OP_Pair   <$> read t2 a2 <*> read t1 a1
+    read (SingleTuple _t)  (OP_Scalar arr) = OP_Scalar <$> readArrayPrim volatile arr ix
+
+readArrayPrim :: Volatile -> Operand e -> Operand Int -> CodeGen (Operand e)
+readArrayPrim volatile arr i = do
+  ptr   <- instr $ GetElementPtr arr [i]
+  v     <- instr $ Load volatile ptr
+  return v
+
+
 -- | Write a value into an array at the given index
 --
 writeArray :: forall sh e. IRArray (Array sh e) -> IR Int -> IR e -> CodeGen ()
-writeArray (IRArray (IR arrs)) (op integralType -> ix) (IR val) =
-  writeArrayData NonVolatile ix (eltType (undefined::e)) arrs val
+writeArray (IRArray (IR adata)) (op integralType -> ix) (IR val) =
+  writeArrayData NonVolatile ix (eltType (undefined::e)) adata val
 
 
 writeArrayData :: Volatile -> Operand Int -> TupleType t -> Operands t -> Operands t -> CodeGen ()
