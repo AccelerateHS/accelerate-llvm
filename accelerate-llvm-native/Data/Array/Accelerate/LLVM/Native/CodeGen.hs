924fc450e02c8cc0c64c72b1e1fe355db419d884
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2015-12-01 18:39:09 -0500

    native-codegen: move some old untyped modules out of the way

--- accelerate-llvm-native/Data/Array/Accelerate/LLVM/Native/CodeGen.hs
+++ accelerate-llvm-native/Data/Array/Accelerate/LLVM/Native/CodeGen.hs
@@ -20,13 +20,11 @@ import Data.Array.Accelerate.LLVM.Native.Target
 import Data.Array.Accelerate.LLVM.Native.CodeGen.Generate
 import Data.Array.Accelerate.LLVM.Native.CodeGen.Map
 -- import Data.Array.Accelerate.LLVM.Native.CodeGen.Permute
--- import Data.Array.Accelerate.LLVM.Native.CodeGen.Transform
 
 
 instance Skeleton Native where
   map _         = mkMap
   generate _    = mkGenerate
---  transform _   = mkTransform
 --  fold _        = mkFold
 --  fold1 _       = mkFold1
 --  permute _     = mkPermute
