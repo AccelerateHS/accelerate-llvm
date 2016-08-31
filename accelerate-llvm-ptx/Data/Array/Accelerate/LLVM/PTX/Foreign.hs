735edfa893a84c8cc48f77e5fed0b7e0d40cc263
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2016-08-31 15:29:42 +1000

    ptx: add basic FFI implementation

--- accelerate-llvm-ptx/Data/Array/Accelerate/LLVM/PTX/Foreign.hs
+++ accelerate-llvm-ptx/Data/Array/Accelerate/LLVM/PTX/Foreign.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 -- |
 -- Module      : Data.Array.Accelerate.LLVM.PTX.Foreign
@@ -9,12 +11,58 @@
 -- Portability : non-portable (GHC extensions)
 --
 
-module Data.Array.Accelerate.LLVM.PTX.Foreign
-  where
+module Data.Array.Accelerate.LLVM.PTX.Foreign (
+
+  -- Foreign functions
+  ForeignAcc(..),
+  ForeignExp(..),
+
+  -- useful re-exports
+  PTX,
+  liftIO,
+
+) where
+
+import qualified Data.Array.Accelerate.Array.Sugar                  as S
+
+import Data.Array.Accelerate.LLVM.State
+import Data.Array.Accelerate.LLVM.CodeGen.Sugar
 
 import Data.Array.Accelerate.LLVM.Foreign
-import Data.Array.Accelerate.LLVM.PTX.Target
+import Data.Array.Accelerate.LLVM.PTX.Target                        ( PTX )
+import Data.Array.Accelerate.LLVM.PTX.Execute.Async                 ( Stream )
+
+import Control.Monad.State
+import Data.Typeable
 
 
-instance Foreign PTX
+instance Foreign PTX where
+  foreignAcc _ (ff :: asm (a -> b))
+    | Just (ForeignAcc _ asm :: ForeignAcc (a -> b)) <- cast ff = Just asm
+    | otherwise                                                 = Nothing
+
+  foreignExp _ (ff :: asm (x -> y))
+    | Just (ForeignExp _ asm :: ForeignExp (x -> y)) <- cast ff = Just asm
+    | otherwise                                                 = Nothing
+
+instance S.Foreign ForeignAcc where
+  strForeign (ForeignAcc s _) = s
+
+instance S.Foreign ForeignExp where
+  strForeign (ForeignExp s _) = s
+
+
+-- Foreign functions in the PTX backend.
+--
+data ForeignAcc f where
+  ForeignAcc :: String
+             -> (Stream -> a -> LLVM PTX b)
+             -> ForeignAcc (a -> b)
+
+-- Foreign expressions in the PTX backend.
+--
+data ForeignExp f where
+  ForeignExp :: String
+             -> IRFun1 PTX () (x -> y)
+             -> ForeignExp (x -> y)
 
