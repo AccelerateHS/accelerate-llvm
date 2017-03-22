414fa1821bfd8acfdfd33b5a7799f46abef5c471
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2017-03-22 11:22:59 +1100

    Add dummy modules for ELF and COFF object linkers

---
+++ accelerate-llvm-native/Data/Array/Accelerate/LLVM/Native/Link/COFF.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE TemplateHaskell #-}
+-- |
+-- Module      : Data.Array.Accelerate.LLVM.Native.Link.COFF
+-- Copyright   : [2017] Trevor L. McDonell
+-- License     : BSD3
+--
+-- Maintainer  : Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+--
+
+module Data.Array.Accelerate.LLVM.Native.Link.COFF (
+
+  loadObject,
+
+) where
+
+import Data.Array.Accelerate.Error
+import Data.Array.Accelerate.LLVM.Native.Link.Object
+
+import Data.ByteString                                    ( ByteString )
+
+
+-- Dynamic object loading
+-- ----------------------
+
+-- Load a COFF object file and return pointers to the executable functions
+-- defined within. The executable sections are aligned appropriately, as
+-- specified in the object file, and are ready to be executed on the target
+-- architecture.
+--
+loadObject :: ByteString -> IO (FunctionTable, ObjectCode)
+loadObject _obj =
+  $internalError "loadObject" "not implemented yet"
+
