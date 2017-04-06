d4b5e315f5ae0eaaa2a1803581b85203ead37f3d
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2017-04-06 19:03:06 +1000

    ptx: rename libdevice modules

--- accelerate-llvm-ptx/Data/Array/Accelerate/LLVM/PTX/Compile.hs
+++ accelerate-llvm-ptx/Data/Array/Accelerate/LLVM/PTX/Compile.hs
@@ -47,7 +47,7 @@ import Data.Array.Accelerate.LLVM.Util
 
 import Data.Array.Accelerate.LLVM.PTX.Analysis.Launch
 import Data.Array.Accelerate.LLVM.PTX.CodeGen
-import Data.Array.Accelerate.LLVM.PTX.Compile.Link
+import Data.Array.Accelerate.LLVM.PTX.Compile.Libdevice
 import Data.Array.Accelerate.LLVM.PTX.Foreign                       ( )
 import Data.Array.Accelerate.LLVM.PTX.Target
 
