4163e2ddac27c7a55450e6225b4ce45e490f8e1b
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2017-11-15 11:01:10 +1100

    add nofib test suite

---
+++ accelerate-llvm-ptx/test/nofib/Main.hs
@@ -0,0 +1,18 @@
+-- |
+-- Module      : nofib-llvm-ptx
+-- Copyright   : [2017] Trevor L. McDonell
+-- License     : BSD3
+--
+-- Maintainer  : Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+--
+
+module Main where
+
+import Data.Array.Accelerate.Test.NoFib
+import Data.Array.Accelerate.LLVM.PTX
+
+main :: IO ()
+main = nofib runN
+
