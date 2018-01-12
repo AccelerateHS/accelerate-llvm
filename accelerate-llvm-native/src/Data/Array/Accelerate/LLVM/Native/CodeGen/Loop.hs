826b73f92e5d2f6c4d43e2ac503fe85d04b3b984
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2018-01-12 13:23:30 +1100

    native/cg: add helper to generate nested for loops

--- accelerate-llvm-native/src/Data/Array/Accelerate/LLVM/Native/CodeGen/Loop.hs
+++ accelerate-llvm-native/src/Data/Array/Accelerate/LLVM/Native/CodeGen/Loop.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
 -- |
 -- Module      : Data.Array.Accelerate.LLVM.CodeGen.Native.Loop
 -- Copyright   : [2014..2017] Trevor L. McDonell
@@ -12,7 +15,10 @@ module Data.Array.Accelerate.LLVM.Native.CodeGen.Loop
   where
 
 -- accelerate
+import Data.Array.Accelerate.Analysis.Match
 import Data.Array.Accelerate.Array.Sugar
+import Data.Array.Accelerate.Error
+import Data.Array.Accelerate.Type
 
 import Data.Array.Accelerate.LLVM.CodeGen.Arithmetic
 import Data.Array.Accelerate.LLVM.CodeGen.IR
@@ -30,6 +36,35 @@ imapFromTo
     -> CodeGen ()
 imapFromTo start end body =
   Loop.imapFromStepTo start (lift 1) end body
+
+
+-- | Generate a series of nested 'for' loops which iterate between the start and
+-- end indices of a given hyper-rectangle. LLVM is very good at vectorising
+-- these kinds of nested loops, but not so good at vectorising the flattened
+-- representation utilising to/from index.
+--
+imapNestedFromTo
+    :: forall sh. Shape sh
+    => IR sh                                    -- ^ initial index (inclusive)
+    -> IR sh                                    -- ^ final index (exclusive)
+    -> (IR sh -> CodeGen ())                    -- ^ apply at each index
+    -> CodeGen ()
+imapNestedFromTo (IR start) (IR end) body = go (eltType (undefined::sh)) start end (body . IR)
+  where
+    go :: TupleType t -> Operands t -> Operands t -> (Operands t -> CodeGen ()) -> CodeGen ()
+    go UnitTuple OP_Unit OP_Unit k
+      = k OP_Unit
+
+    go (PairTuple tsh tsz) (OP_Pair ssh ssz) (OP_Pair esh esz) k
+      | SingleTuple t <- tsz
+      , Just Refl     <- matchScalarType t (scalarType :: ScalarType Int)
+      = go tsh ssh esh
+      $ \sz      -> imapFromTo (IR ssz) (IR esz)
+      $ \(IR i)  -> k (OP_Pair sz i)
+
+    go _ _ _ _
+      = $internalError "imapNestedFromTo" "expected shape with Int components"
+
 
 -- | Iterate with an accumulator between the start and end index, executing the
 -- given function at each.
