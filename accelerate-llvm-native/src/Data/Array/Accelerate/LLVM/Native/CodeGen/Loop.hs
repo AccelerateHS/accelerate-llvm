4a930eb75eaf8cae9e2e09d198e5f63b752657b7
Josh Meredith <joshmeredith2008@gmail.com>
2018-04-10 20:29:00 +1000

    Add stepped variant of native imapNestedFromTo

--- accelerate-llvm-native/src/Data/Array/Accelerate/LLVM/Native/CodeGen/Loop.hs
+++ accelerate-llvm-native/src/Data/Array/Accelerate/LLVM/Native/CodeGen/Loop.hs
@@ -69,6 +69,37 @@ imapNestedFromTo (IR start) (IR end) extent body = go (eltType (undefined::sh)) 
     go _ _ _ _
       = $internalError "imapNestedFromTo" "expected shape with Int components"
 
+-- | Generate a series of nested 'for' loops which iterate between the start and
+-- end indices of a given hyper-rectangle. LLVM is very good at vectorising
+-- these kinds of nested loops, but not so good at vectorising the flattened
+-- representation utilising to/from index.
+--
+imapNestedFromStepTo
+    :: forall sh. Shape sh
+    => IR sh                                    -- ^ initial index (inclusive)
+    -> IR sh                                    -- ^ steps
+    -> IR sh                                    -- ^ final index (exclusive)
+    -> IR sh                                    -- ^ total array extent
+    -> (IR sh -> IR Int -> CodeGen ())          -- ^ apply at each index
+    -> CodeGen ()
+imapNestedFromStepTo (IR start) (IR steps) (IR end) extent body = go (eltType (undefined::sh)) start steps end (body' . IR)
+  where
+    body' ix = body ix =<< intOfIndex extent ix
+
+    go :: TupleType t -> Operands t -> Operands t -> Operands t -> (Operands t -> CodeGen ()) -> CodeGen ()
+    go TypeRunit OP_Unit OP_Unit OP_Unit k
+      = k OP_Unit
+
+    go (TypeRpair tsh tsz) (OP_Pair ssh ssz) (OP_Pair sts stz) (OP_Pair esh esz) k
+      | TypeRscalar t <- tsz
+      , Just Refl     <- matchScalarType t (scalarType :: ScalarType Int)
+      = go tsh ssh sts esh
+      $ \sz      -> Loop.imapFromStepTo (IR ssz :: IR Int) (IR stz) (IR esz)
+      $ \(IR i)  -> k (OP_Pair sz i)
+
+    go _ _ _ _ _
+      = $internalError "imapNestedFromTo" "expected shape with Int components"
+
 
 -- | Iterate with an accumulator between the start and end index, executing the
 -- given function at each.
